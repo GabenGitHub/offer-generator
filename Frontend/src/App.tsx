4ded732da88eb4461aad913ecd8c01b7b87e9931
Gabor Szabo <gabor.szabo86@hotmail.com>
2021-07-11 14:58:26 +0200

    Add basic map to frontend, add geojson data, add react-leaflet packages

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { LatLngExpression } from "leaflet";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import hungary from "./data/counties.json"
+
+import logo from "./assets/images/logo-b.png";
+import "./App.css";
+import "leaflet/dist/leaflet.css";
+
+const App: React.FC = () => {
+    useEffect(() => {
+        console.log(hungary);
+    }, [])
+
+    const position: LatLngExpression = [47.481, 18.990]
+
+    return (
+        <>
+            <header className="App-header">
+                <img src={logo} alt="logo" />
+            </header>
+            <div className="App">
+                <h1>Árajánlat</h1>
+                <MapContainer  className="map-container" center={position} zoom={7} scrollWheelZoom={true}>
+                    <TileLayer
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+                    />
+                    <Marker position={position}>
+                        <Popup>
+                            A pretty CSS3 popup. <br /> Easily customizable.
+                        </Popup>
+                    </Marker>
+                </MapContainer>
+            </div>
+        </>
+    );
 }
 
 export default App;
