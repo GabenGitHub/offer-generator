a4f29edb983f63ced93c23abc00c86f9771ef340
Gabor Szabo <gabor.szabo86@hotmail.com>
2021-08-19 16:29:44 +0200

    OX-4 Add admin, login, reg pages, add menu, styles

---
+++ src/Frontend/src/components/Menu.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+
+const Menu = () => {
+    const history = useHistory();
+
+    const [openNavbar, setOpenNavbar] = useState<boolean>(false);
+    const [fixedNavbar] = useState<boolean>(false);
+    const [navbarColor] = useState<string>("black");
+
+    const toggleClass = () => {
+        if (!openNavbar) {
+            setOpenNavbar(true);
+        } else {
+            setOpenNavbar(false);
+        }
+    };
+
+    const openNavbarClass: string = openNavbar ? "open" : "";
+    const fixedNavbarClass: string = fixedNavbar ? "fixed" : "";
+    const navbarClasses: string = `${openNavbarClass} ${fixedNavbarClass}`;
+    
+    return (
+        <nav className={`navbar ${navbarClasses}`} id="navbar">
+            <div className="nav-center">
+                <div className="nav-header">
+                    <button
+                        type="button"
+                        className={`nav-toggle ${openNavbarClass}`}
+                        id="nav-toggle"
+                        aria-label="nav toggler"
+                        onClick={() => toggleClass()}
+                    >
+                        <div className={`menu-btn-burger ${navbarColor}`}></div>
+                    </button>
+                </div>
+                <div
+                    className={openNavbar ? "nav-links show-links" : "nav-links"}
+                    id="nav-links"
+                >
+                    <button className="nav-link" onClick={() => history.push(`/login`)}>Bejelentkezés</button>
+                    <button className="nav-link" onClick={() => history.push(`/registration`)}>Regisztrálás</button>
+                    <button className="nav-link" onClick={() => history.push(`/admin`)}>Adminisztráció</button>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Menu;
