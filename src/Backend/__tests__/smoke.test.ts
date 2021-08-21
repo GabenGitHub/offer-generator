74d91678e5cb243df978ff34519822ba5e6b051f
Gabor Szabo <gabor.szabo86@hotmail.com>
2021-08-21 21:13:58 +0200

    OX-15 Add basic backend tests

---
+++ src/Backend/__tests__/smoke.test.ts
@@ -0,0 +1,44 @@
+import app from '../server';
+import { dbConnect, dbDisconnect, dbDelete } from './utils/dbHandler.utils';
+import mongoose from 'mongoose';
+import supertest from 'supertest';
+const request = supertest(app);
+
+
+describe("Smoke tests", () => {
+    it("Jest works", async () => {
+        expect(1).toBe(1);
+    });
+
+    it("Supertest works", async () => {
+        const response = await request.get("/invalid_endpoint");
+
+        expect(response.status).toBe(404);
+    });
+
+    it("Database works", async () => {
+        const mongoServer = await dbConnect();
+
+        const TestModel = mongoose.model(
+            "test",
+            new mongoose.Schema({
+                name: String,
+            })
+        );
+
+        const test = new TestModel({
+            name: "testName",
+        });
+        await test.save();
+
+        const doc = await TestModel.findOne();
+        await dbDelete([TestModel]);
+        const result = await TestModel.countDocuments();
+
+        // @ts-ignore
+        expect(doc.name).toBe("testName");
+        expect(result).toBe(0);
+
+        await dbDisconnect();
+    });
+});
