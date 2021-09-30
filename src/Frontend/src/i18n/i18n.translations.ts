414ec1af8c2017da336a8e2df3a4493460d2a254
harikae86 <harmath.esztike@gmail.com>
2021-09-30 10:21:28 +0200

    OX-26 Add i18n initial setup

---
+++ src/Frontend/src/i18n/i18n.translations.ts
@@ -0,0 +1,27 @@
+import { namespaces } from './i18n.constants';
+
+export const hu = {
+    [namespaces.common]: {
+        buttons: {
+            ok: 'Aceptar',
+            cancel: 'Cancelar',
+        },
+    },
+    [namespaces.pages.hello]: {
+        welcome: 'Üdvözöljük',
+        registration: 'Regisztrálás'
+    },
+};
+
+export const en = {
+    [namespaces.common]: {
+        buttons: {
+            ok: 'Ok',
+            cancel: 'Cancel',
+        },
+    },
+    [namespaces.pages.hello]: {
+        welcome: 'Welcome',
+        registration: 'Registration',
+    },
+};
